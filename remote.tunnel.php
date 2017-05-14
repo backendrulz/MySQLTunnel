0b12b7ed24e8d3efff1478e7f15cc23469451916
Cristian Ferreyra <cristian@8d2media.com>
2017-05-14 17:10:22 -0300

    PSR2

--- remote.tunnel.php
+++ remote.tunnel.php
@@ -1,130 +1,105 @@
 <?php
 error_reporting(0);
 
-class MysqlTunnelServer{
-	private $db, $link;
-	public $errors = array();
-	public $ips = array('127.0.0.1'); //Add here allowed ip
+class MysqlTunnelServer
+{
+    private $db;
+    private $link;
+    public $errors = array();
+    public $ips = array('127.0.0.1'); //Add here allowed ip
 
-	function __construct()
-	{
-		$input = file_get_contents('php://input');
+    public function __construct()
+    {
+        $input = file_get_contents('php://input');
 
-		if(!in_array($this->get_ip(), $this->ips) OR empty($input))
-		{
-			$this->show_404();
-		}
+        if (!in_array($this->get_ip(), $this->ips) or empty($input)) {
+            $this->show_404();
+        }
 
-		try{
-			$data			= json_decode($input);
-			$this->db		= $data->db;
-			$this->query	= base64_decode($data->query);
-		}catch (Exception $e){
-			$this->errors[] = $e->getMessage();
-		}
+        try {
+            $data = json_decode($input);
+            $this->db = $data->db;
+            $this->query = base64_decode($data->query);
+        } catch (Exception $e) {
+            $this->errors[] = $e->getMessage();
+        }
 
-		$this->tunnel_connect();
-		$this->get_result();
+        $this->tunnel_connect();
+        $this->get_result();
+    }
 
-	}
+    private function tunnel_connect()
+    {
+        $this->link = new mysqli($this->db->host, $this->db->user, $this->db->pass, $this->db->db_name, $this->db->port);
 
-	private function tunnel_connect()
-	{
-		$this->link = new mysqli($this->db->host, $this->db->user, $this->db->pass, $this->db->db_name, $this->db->port);
+        if ($this->link->connect_error) {
+            $this->errors[] = $this->link->connect_errno . ' - '. $this->link->connect_error;
+            return false;
+        }
+    }
 
-		if ($this->link->connect_error) {
+    private function do_query()
+    {
+        $result = $this->link->query($this->query);
 
-			$this->errors[] = $this->link->connect_errno . ' - '. $this->link->connect_error;
-			return false;
-		}
+        if ($result) {
+            $num_rows = $result->num_rows;
 
-	}
+            $new_result = array();
+            $new_result['num_rows'] = $num_rows;
 
-	private function do_query()
-	{
-		$result = $this->link->query($this->query);
-
-		if($result)
-		{
-			$num_rows = $result->num_rows;
-
-			$new_result = array();
-			$new_result['num_rows'] = $num_rows;
-
-			if($num_rows > 1)
-			{
-				while($row = $result->fetch_object())
-				{
-					$new_result['result'][] = $row;
-				}
-
-			}else{
-				$new_result['result'] = $result->fetch_object();
-
-			}
+            if ($num_rows > 1) {
+                while ($row = $result->fetch_object()) {
+                    $new_result['result'][] = $row;
+                }
+            } else {
+                $new_result['result'] = $result->fetch_object();
+            }
             $result->close();
 
-			return json_encode($new_result);
+            return json_encode($new_result);
+        } else {
+            $this->errors[] = $this->link->error;
+            return false;
+        }
+    }
 
-		}
-		else
-		{
-			$this->errors[] = $this->link->error;
-			return false;
-		}
+    public function get_result()
+    {
+        $result = $this->do_query();
 
-	}
+        if (count($this->errors) > 0) {
+            echo $this->get_errors();
+        } else {
+            echo $result;
+        }
+    }
 
-	public function get_result()
-	{
-		$result = $this->do_query();
+    private function get_errors()
+    {
+        return json_encode(array('errors' => array_unique($this->errors)));
+    }
 
-		if(count($this->errors) > 0)
-		{
-			echo $this->get_errors();
+    private function get_ip()
+    {
+        if ($_SERVER['HTTP_HOST'] == 'localhost') {
+            $ip = '127.0.0.1';
+        } elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
 
-		}else
-		{
-			echo $result;
+        return $ip;
+    }
 
-		}
-
-	}
-
-	private function get_errors()
-	{
-		return json_encode(array('errors' => array_unique($this->errors)));
-	}
-
-	private function get_ip()
-	{
-		if($_SERVER['HTTP_HOST'] == 'localhost' )
-		{
-			$ip = '127.0.0.1';
-		}
-		elseif(isset($_SERVER['HTTP_CLIENT_IP']))
-		{
-			$ip = $_SERVER['HTTP_CLIENT_IP'];
-		}
-		elseif(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-		{
-			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-		}
-		else
-		{
-			$ip = $_SERVER['REMOTE_ADDR'];
-		}
-
-		return $ip;
-
-	}
-
-	private function show_404()
-	{
-		header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');
-		die;
-	}
-
+    private function show_404()
+    {
+        header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');
+        die;
+    }
 }
 
 new MysqlTunnelServer;
--- tunnel.class.php
+++ tunnel.class.php
@@ -2,129 +2,122 @@
 
 class MysqlTunnelClient
 {
-	public $url, $errors = array(), $num_rows = 0;
-	private $dbData, $result;
+    public $url;
+    public $errors = array();
+    public $num_rows = 0;
+    private $dbData;
+    private $result;
 
-	function __construct($cfg)
-	{
-		$this->url		= $cfg['tunnel_url'];
-		$this->dbData	= $cfg['db_data'];
-	}
+    public function __construct($cfg)
+    {
+        $this->url = $cfg['tunnel_url'];
+        $this->dbData = $cfg['db_data'];
+    }
 
-	private function sendQuery($query, $array = FALSE)
-	{
-		$query = base64_encode($query);
+    private function sendQuery($query, $array = false)
+    {
+        $query = base64_encode($query);
 
-		$data = json_encode(array('db' => $this->dbData, 'query' => $query));
+        $data = json_encode(array('db' => $this->dbData, 'query' => $query));
 
-		$link = $this->makeRequest($this->url, $data);
+        $link = $this->makeRequest($this->url, $data);
 
-		$this->result = json_decode($link, TRUE);
-		$this->num_rows = isset($this->result['num_rows']) ? $this->result['num_rows'] : 0;
+        $this->result = json_decode($link, true);
+        $this->num_rows = isset($this->result['num_rows']) ? $this->result['num_rows'] : 0;
 
-		if(isset($this->result['errors']))
-		{
-			$this->errors();
-		}
+        if (isset($this->result['errors'])) {
+            $this->errors();
+        }
 
-		return $this->result['result'];
+        return $this->result['result'];
+    }
 
-	}
+    public function query($query)
+    {
+        $this->sendQuery($query, true);
+        return $this;
+    }
 
-	public function query($query)
-	{
-		$this->sendQuery($query, TRUE);
-		return $this;
-	}
+    public function get($table_name, $limit = null, $offset = null)
+    {
+        $sql = "SELECT * FROM {$table_name}";
 
-	public function get($table_name, $limit = NULL, $offset = NULL)
-	{
-		$sql = "SELECT * FROM {$table_name}";
+        if (isset($limit) || isset($offset) && isset($limit)) {
+            if ($offset == 0) {
+                $offset = '';
+            } else {
+                $offset .= ', ';
+            }
 
-		if(isset($limit) || isset($offset) && isset($limit))
-		{
-			if ($offset == 0)
-			{
-				$offset = '';
+            $sql .= ' LIMIT '.$offset.$limit;
+        }
 
-			}else{
-				$offset .= ', ';
+        return $this->sendQuery($sql, true);
+    }
 
-			}
+    public function num_rows()
+    {
+        return $this->num_rows;
+    }
 
-			$sql .= ' LIMIT '.$offset.$limit;
-		}
+    public function row($index = 0)
+    {
+        if (!isset($this->result['result'][$index])) {
+            return false;
+        }
 
-		return $this->sendQuery($sql, TRUE);
+        return ($this->num_rows > 1) ? $this->result['result'][$index]: $this->result['result'];
+    }
 
-	}
+    public function fetch_array()
+    {
+        return $this->result['result'];
+    }
 
-	public function num_rows()
-	{
-		return $this->num_rows;
-	}
+    public function fetch_object()
+    {
+        return (object) $this->result['result'];
+    }
 
-	public function row( $index = 0 )
-	{
-		if(!isset($this->result['result'][$index])) return FALSE;
+    public function update($table, $values, $where = array(), $limit = false)
+    {
+        foreach ($values as $key => $val) {
+            $valstr[] = $key . ' = ' . $val;
+        }
 
-		return ($this->num_rows > 1) ? $this->result['result'][$index]: $this->result['result'];
-	}
+        foreach ($where as $key => $val) {
+            $wherestr[] = $key . ' = "' . $val . '"';
+        }
 
-	public function fetch_array()
-	{
-		return $this->result['result'];
-	}
+        $limit = (!$limit) ? '' : ' LIMIT '.$limit;
 
-	public function fetch_object()
-	{
-		return (object) $this->result['result'];
-	}
+        $sql = 'UPDATE '.$table.' SET '.implode(', ', $valstr);
+        $sql .= ($where != '' and count($where) >=1) ? ' WHERE '.implode(', ', $wherestr) : '';
+        $sql .= $orderby.$limit;
 
-	public function update($table, $values, $where = array(), $limit = FALSE)
-	{
-		foreach ($values as $key => $val)
-		{
-			$valstr[] = $key . ' = ' . $val;
-		}
-
-		foreach ($where as $key => $val)
-		{
-			$wherestr[] = $key . ' = "' . $val . '"';
-		}
-
-		$limit = (!$limit) ? '' : ' LIMIT '.$limit;
-
-		$sql = 'UPDATE '.$table.' SET '.implode(', ', $valstr);
-		$sql .= ($where != '' AND count($where) >=1) ? ' WHERE '.implode(', ', $wherestr) : '';
-		$sql .= $orderby.$limit;
-
-		return $sql;
-	}
+        return $sql;
+    }
 
     private function errors()
-	{
-		foreach($this->result['errors'] as $error)
-		{
-			echo $error."\n<br />";
-		}
-		die;
-	}
+    {
+        foreach ($this->result['errors'] as $error) {
+            echo $error."\n<br />";
+        }
+        die;
+    }
 
     private function makeRequest($url, $data)
     {
         $ch = curl_init($url);
 
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-		curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		// curl_setopt($ch, CURLOPT_VERBOSE, true);
+        // curl_setopt($ch, CURLOPT_VERBOSE, true);
 
         return curl_exec($ch);
-
     }
-
 }
 
 /* end of file */
